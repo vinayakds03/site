cd889d27041f9bc2229858c365268e3438efbf16
vinayakds03 <118668418+vinayakds03@users.noreply.github.com>
2025-04-22 10:46:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('downloadBtn').addEventListener('click', function () {
     const link = document.createElement('a');
-    link.href = './public/file.pdf';  // Adjust path if needed
-    link.download = 'file.pdf';
+    link.href = './public/file.zip';  // Adjust path if needed
+    link.download = 'file.zip';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
